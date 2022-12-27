8030bfe075126fd0c4461291b5ee7b9da532f9b0
noman-swe <noman.swe130@gmail.com>
2022-12-27 16:55:06 +0600

    39-3 Location, URL parts, query string, href, hash, assign, reload

---
+++ Module-39/BrowserAPI/39-3 Location, URL parts, query string, href, hash, assign, reload/location.js
@@ -0,0 +1,18 @@
+// kono website e giye console e location likhe enter korle oi site er full address details chole ashbe.  
+const refresh = () => {
+    location.reload();
+}
+
+// 
+const visitPH = () => {
+    location.assign('https://web.programming-hero.com/');
+}
+
+// 
+const messiGoal = () => {
+    location.assign('https://www.youtube.com/watch?v=G1txwF6YsuM&t=478s');
+    location.search();//finds the querry
+    location.hash();//finds the hashes in the site
+}
+
+// and there is many more thing.. 'location'
