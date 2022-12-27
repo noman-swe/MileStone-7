1083754ba9ccf8ca3691294d8b05950bd1928b7b
noman-swe <noman.swe130@gmail.com>
2022-12-27 17:30:57 +0600

    39-4 History api to navigate back, forward, go, history length

---
+++ Module-39/BrowserAPI/39-4 History/history.js
@@ -0,0 +1,14 @@
+console.log(window.history === history);
+// history -  history object contains an array of URLs visited by the user. 
+history.length
+history.back() //
+history.forward()
+
+// history
+history.go(-1)//ekta back jabe.
+history.go(-5)// 5 ta back jabe.
+history.go(4)//forward e jabe 4 step.
+
+history.go()// page will refresh
+
+// ekta tap er moddh agee kothay kothay silam ar kotay kothay jete pari seta history diye korajay
