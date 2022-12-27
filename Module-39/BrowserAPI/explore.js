6eb321761edd445094150922bdd6d36fdc3aae46
noman-swe <noman.swe130@gmail.com>
2022-12-27 08:11:58 +0600

    39-1 Module Overview, BOM vs DOM, Browser API

---
+++ Module-39/BrowserAPI/explore.js
@@ -0,0 +1,10 @@
+const name = 15;
+const student = {
+    name: 'Jolil', friend: 'borsha', job: 'business',
+    makeMovie: function (name) {
+        console.log(name);
+    }
+}
+document.getElementById('name');
+
+// it will have an error because of it is not connected with the browser.
