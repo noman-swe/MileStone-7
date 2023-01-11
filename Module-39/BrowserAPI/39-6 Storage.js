100844720df6bf73768941aa71477e4080071661
noman-swe <noman.swe130@gmail.com>
2023-01-11 10:17:57 +0600

    39-7 Dynamically set and read from local, session storage

---
+++ Module-39/BrowserAPI/39-6 Storage.js
@@ -0,0 +1,9 @@
+/*
+what is session storage and local storage?
+-sessionStorage is similar to localStorage ; the difference is that while data in localStorage doesn't expire, data in sessionStorage is cleared when the page session ends
+*/
+
+window.sessionStorage
+localStorage
+window.localStorage.getItem()
+window.localStorage.setItem('def', 777) //setItem() use kore local storage and session storage er maaan/value set kora hoy
