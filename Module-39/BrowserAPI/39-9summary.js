23c33bcad33b5c9d2e844f5201859c1c00645435
noman-swe <noman.swe130@gmail.com>
2023-01-22 23:21:26 +0600

    39 done

---
+++ Module-39/BrowserAPI/39-9summary.js
@@ -0,0 +1,24 @@
+alert('');
+confirm('are you there?');
+prompt('name');
+
+
+/* 
+location.reload()
+location.href = '';
+location.hash = ;
+http://127.0.0.1:5500/39-7-excercise/index.html
+*/
+
+/* 
+history.back();
+history.go()
+*/
+
+
+
+//cookies
+// sessionStorage vs localStorage
+//cookies vs sessionStorage vs localStorage
+
+//---->4min
