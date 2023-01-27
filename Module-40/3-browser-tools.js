c65a8fede946f3d34a86f5cef948fd4d2f70593d
noman-swe <noman.swe130@gmail.com>
2023-01-27 11:39:19 +0600

    40-3 Console tab, preserve log, log levels, console api

---
+++ Module-40/3-browser-tools.js
@@ -0,0 +1,29 @@
+console.log('it is console log.');
+
+console.info('console-info');
+
+console.warn('do not come here.');
+
+console.clear() // clear(); -> all clear hoye jabe
+
+console.error('wrong password.');
+
+
+// monitor(VariableName) -> only works on browser console. & off korar jonne unmonitor(variableName)
+
+/*
+browser e jodi likhi--
+----------
+const phonePlus = ${phone-plus}
+-> button
+monitorEvents(phonePlus); // ei button e joto event kora hoisy oi sob e monitor korbe. 
+
+-->> unmonitorEvents(variable) // unmonitor hoye jabe
+
+-->> monitorEvents() er moddhy aro pera meter dewa jay --- like--
+monitorEvents(variablename, ['eventName', 'anotherEventName']);
+*/
+
+console.count('function k koto ber cl kora hoisy seta count kore dibe')
+
+// chorme console api & utilities ---search about this
