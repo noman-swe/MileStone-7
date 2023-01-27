80c7bc3327738a61d68900b03d07f0b921fb56eb
noman-swe <noman.swe130@gmail.com>
2023-01-27 16:14:31 +0600

    40-4 Sources tab, break point, call stack, event break

---
+++ Module-40/4-browser-tools-sources.js
@@ -0,0 +1,11 @@
+// smippet add kore tate code likhe rakha jay
+/* 
+1.cntrl+g then line no likhle direct oi line e jawa jay
+2.control+shift+o diye function khule ber kora jay
+3.debugger kora jabe -watch kora jbe
+4.eventListenerBrrakPoint use kora jbe
+5.source e Esc press korle arekta console pannel chole ashy
+6. prettyPrint er use
+
+
+*/
